48674e649a23ec6d00306ff889870aa09d0e544a
Hoang Long <bhlong1911@gmail.com>
2026-08-06 08:52:04 +0700

    Day 77: FastAPI path params, query params, type validation

--- fastapi-pnl/main.py
+++ fastapi-pnl/main.py
@@ -13,3 +13,19 @@ def hello():
 @app.get("/about")
 def about():
     return {"name":"Crypto P&L Tracker", "version":"v1.0", "feature":"Tracking crypto position & Calculating ROI", "author":"Aeron"}
+
+@app.get("/coin/{coin_id}")
+def get_coin(coin_id):
+    return {"coin": coin_id}
+
+@app.get("/double/{number}")
+def double(number:int):
+    return number * 2
+
+@app.get("/greet")
+def greet(name = "stranger"):
+    return f"Hello, {name}"
+
+@app.get("/coin/{coin_id}/convert")
+def infor(coin_id, currency = "usd"):
+    return {"coin": coin_id, "currency": currency}
